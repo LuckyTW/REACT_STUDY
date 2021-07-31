97c2e5f16b204059fbd0d2f30be21b5d040f221f
LuckyTW <skqkstjr2@gmail.com>
2021-07-31 21:19:13 +0900

    revised

--- React Basics/src/components/Expenses/ExpenseItem.js
+++ React Basics/src/components/Expenses/ExpenseItem.js
@@ -2,18 +2,26 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
+import { useState } from 'react';
+
 function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = 'Car Insurance';
   // const expenseAmount = 294.67;
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('updated!');
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
