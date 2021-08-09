732ec17c8a9456e566abcc00cbd569fe979ad4df
LuckyTW <skqkstjr2@gmail.com>
2021-08-09 11:37:56 +0900

    useEffect done

--- SideEffect,Reducers,Context API/src/components/Login/Login.js
+++ SideEffect,Reducers,Context API/src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
@@ -23,10 +31,6 @@ const Login = (props) => {
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    `    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );`;
   };
 
   const validateEmailHandler = () => {
